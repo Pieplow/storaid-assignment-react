acd88f95006519f7bfc99b6441629131a60b11e0
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-27 14:10:51 +0100

    added icons

--- src/components/layout/SocialBar.jsx
+++ src/components/layout/SocialBar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+
+import {Link } from 'react-router-dom';
 import './SocialBar.css';
 import phoneIcon from '../../assets/phone-icon.svg';
 import emailIcon from '../../assets/e-mail-icon.svg';
 
+import FacebookIcon from "../../assets/Facebook.png";
+import HumbleIcon from "../../assets/Humble.png";  
+import InstagramIcon from "../../assets/Instagram.png";
+import YoutubeIcon from "../../assets/Youtube.png";
 
 
 function SocialBar() {
@@ -15,7 +21,26 @@ function SocialBar() {
       <div className="email">
         <img src={emailIcon} alt="contact by email" />
         <span>contact@domain.com</span>
-      </div>
+        </div>
+        
+    <div className="social-media">
+      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <img src={FacebookIcon} alt="Facebook" />
+      </a>
+
+      <a href="https://humblebundle.com" target="_blank" rel="noopener noreferrer">
+        <img src={HumbleIcon} alt="Humble" />
+      </a>
+
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <img src={InstagramIcon} alt="Instagram" />
+      </a>
+
+      <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+        <img src={YoutubeIcon} alt="YouTube" />
+      </a>
+    </div>
+      
     </div>
   );
 }
