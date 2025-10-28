42d6a30cc35c96638abcefb6cc5462253fc0913f
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-28 14:57:25 +0100

    tested to add in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from './components/sections/Header';
+import Hero from "./components/sections/Hero";
 import "./styles/global.css";
 
 function App() {
   return (
     <>
-      <Header /> {}
-         
-
+      <Header />
+      <Hero />
+    
     </>
   );
 }
--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,8 +1,6 @@
-import heroImg from "@/assets/hero-image.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SectionText from "../../generics/SectionText";
 import Button from "../../generics/Button";
-import './styles/global.css';
 
 
 function Hero() {
