7da5e0fb0d6313cf788c9688a12bedcee57808a0
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-28 14:32:32 +0100

    added button

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,14 +1,17 @@
 import heroImg from "@/assets/hero-image.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SectionText from "../../generics/SectionText";
+import Button from "../../generics/Button";
 import './styles/global.css';
 
 
-function Header() {
+function Hero() {
   return (
     <header className="hero">
       <SectionText />
+      <Button />
     </header>
   );
 }
 
-export default Header;
+export default Hero;
