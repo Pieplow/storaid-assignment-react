5aa86c00668ff220fc375297762cc2e49201318e
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-28 14:30:35 +0100

    added component section header

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,3 +1,14 @@
 import heroImg from "@/assets/hero-image.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
+
+
+function Header() {
+  return (
+    <header className="hero">
+      <SectionText />
+    </header>
+  );
+}
+
+export default Header;
