58943683b8adfdec0d3e37fd830c7c9a70e7a33b
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-28 17:05:22 +0100

    resolve merge problem

--- src/generics/SectionText.jsx
+++ src/generics/SectionText.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./SectionText.css"; 
 
-function SectionText({
+export default function SectionText({
   label = "Welcome To StorAid",
   title = "Space Simplified, Storage Perfected",
   text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 }) {
   return (
-    <div className="section-text">
-      {label && <p className="section-text_label">{label}</p>}
+    <div className="section-heading">
+      <p className="section-text_label">{label}</p>
       <h2 className="section-text_title">{title}</h2>
       <p className="section-text_text">{text}</p>
     </div>
   );
 }
-
-export default SectionText;
