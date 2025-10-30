1664d993ecf2442b539df1aec467dd3674e300a1
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-10-30 13:06:26 +0100

    worked on aligment text

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,15 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SectionText from "../../generics/SectionText";
 import Button from "../../generics/Button";
-
+import heroImage from "../../assets/hero-image.svg";
 
 function Hero() {
   return (
-    <header className="hero">
-        <SectionText
+    <header className="hero-section py-5 text-white">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-6 text-center text-md-start">
+            <SectionText
               title="Space Simplified, Storage Perfected"
               text="Providing a safe space for your treasured items with modern, flexible storage solutions."
             />
+            <Button />
+          </div>
+
+        
+        </div>
+      </div>
     </header>
   );
 }
