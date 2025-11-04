efe9eb7d335b9d7361c8fabc9f4ddac40049ac7b
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-04 20:46:19 +0100

    section done

--- src/components/sections/PriceCards.jsx
+++ src/components/sections/PriceCards.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import PriceTile from "../../generics/PriceTile";
+import SectionText from "../../generics/SectionText";
+import "./PriceCards.css";
 
 const PriceCards = () => {
   return (
-    <div className="container my-5">
+    <section className="section-PriceCards">
+        <div className="container my-5">
+    <SectionText  
+    label="Pricing plan"
+    title="Find the Perfect Plan for Your Storage Needs"
+    text=""
+    />
+    
       <div className="row justify-content-center g-4">
         <div className="col-md-3 col-sm-6 d-flex justify-content-center">
           <PriceTile title="Small Unit" price="50" />
@@ -19,6 +28,7 @@ const PriceCards = () => {
         </div>
       </div>
     </div>
+    </section>
   );
 };
 
