4c945304dd18badcf8d20769f25c3ed786cc171f
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-08 11:55:47 +0100

    added base html

---
+++ src/services/api.jsx
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://win25-jsf-assignment.azurewebsites.net/api';
+
+
+export const sendContact = async (data) => {
+try {
+    
+     } catch (err) {
+    console.error('API error:', err);
+    throw err;
+  }
+}
