7170047c6b08a2c37c4e7a5f392bf986a42f2b7e
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-08 12:27:21 +0100

    added html för subscribe

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,18 @@
 const BASE_URL = 'https://win25-jsf-assignment.azurewebsites.net/api';
 
+export const sendSubscribe = async (data) => {
+  try {
+    const res = await fetch(`${BASE_URL}/subscribe`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
 
-export const sendContact = async (data) => {
-try {
+    if (!res.ok) throw new Error('Kunde inte skicka formulär');
+    return await res.json();
     
-     } catch (err) {
+  } catch (err) {
     console.error('API error:', err);
     throw err;
   }
-}
+};
