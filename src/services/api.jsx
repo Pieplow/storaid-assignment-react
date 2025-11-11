75a72ff6435791a4b2c9beeaccc0ee459e076d36
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-11 02:31:55 +0100

    added more post and get done

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -17,6 +17,22 @@ export const sendSubscribe = async (data) => {
   }
 };
 
+export const sendContact = async (data) => {
+  try {
+    const res = await fetch(`${BASE_URL}/contact`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) throw new Error("Kunde inte skicka kontaktformulär");
+    return await res.json();
+  } catch (err) {
+    console.error("API error (contact):", err);
+    throw err;
+  }
+};
+
+
 //GET//
 export const getFaqs = async () => {
   try {
@@ -25,6 +41,28 @@ export const getFaqs = async () => {
     return await res.json();
   } catch (err) {
     console.error('API error (faqs):', err);
+    throw err;
+  }
+};
+
+export const getTestimonials = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}/testimonials`);
+    if (!res.ok) throw new Error("Kunde inte hämta testimonials");
+    return await res.json();
+  } catch (err) {
+    console.error("API error (testimonials):", err);
+    throw err;
+  }
+};
+
+export const getBlogs = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}/blogs/latest`);
+    if (!res.ok) throw new Error("Kunde inte hämta bloggar");
+    return await res.json();
+  } catch (err) {
+    console.error("API error (blogs):", err);
     throw err;
   }
 };
