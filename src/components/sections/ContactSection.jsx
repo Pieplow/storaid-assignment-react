937a4f6ab734c0ad21b67018ca359638bce69075
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-12 11:20:54 +0100

    added css

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -1,39 +1,35 @@
 import React from "react";
 import ContactForm from "../../generics/ContactForm";
 import SectionText from "../../generics/SectionText";
-import greyImageBooking from "../../assets/grey-image-booking.svg";
-
+import greyImageBooking from "./../assets/grey-image-contact.svg";
 import "./ContactSection.css";
 
 const ContactSection = () => {
   return (
-    <section className="contact-section py-5">
-      <div className="container">
-        <div className="row align-items-center">
-
-         
-          <div className="col-md-6 mb-4 mb-md-0 text-center text-md-start h-100">
+    <section className="contact-section container py-5">
+      <div className="row align-items-center">
+        
+        <div className="col-md-6 text-center text-md-start mb-4 mb-md-0">
+          <div className="section-text-left">
             <SectionText
               label="Get in Touch"
               title="Get Personalized Assistance – Contact Us"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo"
-            />
-
-            <img
-              src={greyImageBooking}
-              alt="Booking illustration"
-              className="img-fluid mt-4 booking-image h-100"
-              width="664"
-              height="337"
-              loading="lazy"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."
             />
           </div>
 
-       
-          <div className="col-md-6 h-100">
+          <img
+            src={greyImageBooking}
+            alt="Booking illustration"
+            className="img-fluid mt-4 booking-image"
+            loading="lazy"
+          />
+        </div>
+
+        <div className="col-md-6">
+          <div className="section-text-right">
             <ContactForm />
           </div>
-
         </div>
       </div>
     </section>
