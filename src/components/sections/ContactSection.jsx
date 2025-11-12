5bafb37dae471fa8dbdcf6f89b3ec0ed5fa4cb32
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-12 11:10:26 +0100

    added images

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ContactForm from "../../generics/ContactForm";
 import SectionText from "../../generics/SectionText";
-import greyImageBooking from "../../assets/grey-image-booking.svg";
+import greyImageBooking from "../../assets/grey-image-contact.svg";
 
 import "./ContactSection.css";
 
