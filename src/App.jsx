fe03739e9f583aff247e7bf87f7e7256229f08de
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-11-14 03:17:30 +0100

    fixed mergin conflict with app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import SocialBar from "./components/layout/SocialBar";
-import Navbar from "./components/layout/Navbar";
-import Newsletter from "./components/sections/Newsletter";
-import Footer from "./components/sections/Footer";
-
+import SocialBar from "./components/layout/SocialBar.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
+import Footer from "./components/sections/Footer.jsx";
+import Newsletter from "./components/sections/Newsletter.jsx";
 
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Services from "./Pages/Services";
-import Contact from "./Pages/Contact";
-import Booking from "./Pages/Booking";
-import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Services from "./Pages/Services.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Booking from "./Pages/Booking.jsx";
+import NotFound from "./Pages/NotFound.jsx";
 
 export default function App() {
   return (
     <>
-      <Header /> {}
-         
+      <SocialBar />
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Newsletter />
+      <Footer />
     </>
   );
 }
